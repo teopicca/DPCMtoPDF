fd6804ad992e171f7745b042546b934115df5c0e
Matteo Piccapietra <matteopiccapietra@gmail.com>
2020-11-23 18:02:44 +0100

    Add files via upload

---
+++ server.py
@@ -0,0 +1,19 @@
+import pdfkit
+from flask import request, render_template, Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/form_data', methods=['POST'])
+def form_data():
+    
+    if request.json:
+        form_data = request.json['form_data']
+
+        template = render_template('autocertificazione_model.html', form_data = form_data)            
+        pdf = pdfkit.from_string(template, 'autocertificazione.pdf')
+
+        return jsonify(status=200)
+    return jsonify(status=200)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
